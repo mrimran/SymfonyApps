fdc578526cf9a6a5c8f47e95ccb433f73ca10ef2
Imran <imran@zeropoint.it>
2018-12-13 15:04:15 +0500

    Introduced basic validation for user.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints;
 
 /**
  * @ApiResource(
@@ -32,22 +33,35 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
+     * @Constraints\NotBlank()
+     * @Constraints\Length(min=3, max=255)
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Constraints\NotBlank()
+     * @Constraints\Length(min=6, max=255)
+     * @Constraints\Regex(
+     *     pattern="/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,}/",
+     *     message="Password must be 6 characters long and contain at least one digit, one upper case letter and one lower case letter."
+     * )
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
+     * @Constraints\NotBlank()
+     * @Constraints\Length(min=3, max=255)
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Constraints\NotBlank()
+     * @Constraints\Email()
+     * @Constraints\Length(min=6, max=255)
      */
     private $email;
 
